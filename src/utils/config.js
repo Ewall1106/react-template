ad5620ab2baedc75ad14dc5b46c66e1d13c51669
xiongwei <>
2022-02-26 11:47:17 +0800

    add

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,16 +1,6 @@
 module.exports = {
   development: {
     baseURL: '/dev-api',
-    mockURL: 'http://127.0.0.1:7001'
-    // mockURL: 'http://101.35.89.91/api'
-  },
-  production: {
-    baseURL: 'http://101.35.89.91/api',
-    mockURL: 'http://101.35.89.91/api'
-  },
-  iframeUrl: ['http://localhost:8080/js/chunk-vendors.js', 'http://localhost:8080/js/app.js']
-  // iframeUrl: [
-  //   'http://101.35.89.91/bamboo-h5-vue/js/chunk-vendors.js',
-  //   'http://101.35.89.91/bamboo-h5-vue/js/app.js'
-  // ]
+    mockURL: 'xxx.xxx.xxx'
+  }
 }
